c52e889ded7f908d6cd6d9ff490de6c4589b5c2c
eleoromario <eleo.romario@gmail.com>
2025-03-06 15:26:37 -0500

    feat: gregar un contenedor para el contenido con clases de estilo dinámicas

--- src/presentation/components/layout/AdminLayout.tsx
+++ src/presentation/components/layout/AdminLayout.tsx
@@ -1,6 +1,7 @@
 import Cookies from "js-cookie";
 
 import { SearchProvider } from "@/shared/context/search-context";
+import { cn } from "@/shared/lib/utils";
 import { SidebarProvider } from "../ui/sidebar";
 import { AppSidebar } from "./AppSidebar";
 import SkipToMain from "./SkipToMain";
@@ -12,7 +13,20 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
       <SidebarProvider defaultOpen={defaultOpen}>
         <SkipToMain />
         <AppSidebar />
-        {children}
+        <div
+          id="content"
+          className={cn(
+            "ml-auto w-full max-w-full",
+            "peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]",
+            "peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]",
+            "transition-[width] duration-200 ease-linear",
+            "flex h-svh flex-col",
+            "group-data-[scroll-locked=1]/body:h-full",
+            "group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh"
+          )}
+        >
+          {children}
+        </div>
       </SidebarProvider>
     </SearchProvider>
   );
