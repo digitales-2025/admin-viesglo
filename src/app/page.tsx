e504fe07e0bab5a3e22ff1a316a8e0e801ba8c9c
eleoromario <eleo.romario@gmail.com>
2025-03-06 15:46:08 -0500

    feat: agregar componentes de encabezado, navegación superior y menú de perfil a la página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,49 @@
 import AdminLayout from "@/presentation/components/layout/AdminLayout";
+import { Header } from "@/presentation/components/layout/Header";
+import { ProfileDropdown } from "@/presentation/components/layout/ProfileDropdown";
+import { Search } from "@/presentation/components/layout/Search";
+import { ThemeSwitch } from "@/presentation/components/layout/ThemeSwitch";
+import { TopNav } from "@/presentation/components/layout/TopNav";
+
+const topNav = [
+  {
+    title: "Overview",
+    href: "dashboard/overview",
+    isActive: true,
+    disabled: false,
+  },
+  {
+    title: "Customers",
+    href: "dashboard/customers",
+    isActive: false,
+    disabled: true,
+  },
+  {
+    title: "Products",
+    href: "dashboard/products",
+    isActive: false,
+    disabled: true,
+  },
+  {
+    title: "Settings",
+    href: "dashboard/settings",
+    isActive: false,
+    disabled: true,
+  },
+];
 
 export default function Home() {
   return (
     <AdminLayout>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-5xl font-bold">Welcome to Admin</h1>
-      </div>
+      {/* ===== Top Heading ===== */}
+      <Header>
+        <TopNav links={topNav} />
+        <div className="ml-auto flex items-center space-x-4">
+          <Search />
+          <ThemeSwitch />
+          <ProfileDropdown />
+        </div>
+      </Header>
     </AdminLayout>
   );
 }
---
+++ src/presentation/components/layout/ThemeSwitch.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect } from "react";
+import { Check, Moon, Sun } from "lucide-react";
+
+import { Button } from "@/presentation/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/presentation/components/ui/dropdown-menu";
+import { useTheme } from "@/shared/context/theme-context";
+import { cn } from "@/shared/lib/utils";
+
+export function ThemeSwitch() {
+  const { theme, setTheme } = useTheme();
+
+  /* Update theme-color meta tag
+   * when theme is updated */
+  useEffect(() => {
+    const themeColor = theme === "dark" ? "#020817" : "#fff";
+    const metaThemeColor = document.querySelector("meta[name='theme-color']");
+    if (metaThemeColor) metaThemeColor.setAttribute("content", themeColor);
+  }, [theme]);
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="scale-95 rounded-full">
+          <Sun className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light <Check size={14} className={cn("ml-auto", theme !== "light" && "hidden")} />
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+          <Check size={14} className={cn("ml-auto", theme !== "dark" && "hidden")} />
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+          <Check size={14} className={cn("ml-auto", theme !== "system" && "hidden")} />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
