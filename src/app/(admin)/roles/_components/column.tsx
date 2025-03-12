2657c32791d778cdea8e30a6d8554a13ee212e91
eleoromario <eleo.romario@gmail.com>
2025-03-12 16:45:31 -0500

    feat: remove checkbox selection from roles table columns

--- src/app/(admin)/roles/_components/column.tsx
+++ src/app/(admin)/roles/_components/column.tsx
@@ -3,36 +3,14 @@
 import { ColumnDef } from "@tanstack/react-table";
 
 import { DataTableColumnHeader } from "@/shared/components/data-table/DataTableColumnHeaderProps";
-import { Checkbox } from "@/shared/components/ui/checkbox";
 import { Role } from "../_types/roles";
 import { RolesTableActions } from "./RolesTableActions";
 
 export const columnsRoles: ColumnDef<Role>[] = [
   {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="translate-y-[2px]"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="translate-y-[2px]"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
     accessorKey: "name",
     header: ({ column }) => <DataTableColumnHeader column={column} title="Nombre" />,
-    cell: ({ row }) => <div className="font-medium">{row.getValue("name")}</div>,
+    cell: ({ row }) => <div className="font-semibold capitalize">{row.getValue("name")}</div>,
   },
   {
     accessorKey: "description",
