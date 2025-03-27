359f3737ebb10cfd17afc42ae335d1d673db77a7
eleoromario <eleo.romario@gmail.com>
2025-03-27 17:20:00 -0500

    feat: refactor ObjectivesList to use objectives from selectedService and update types

--- src/app/(admin)/services/_components/ObjectivesList.tsx
+++ src/app/(admin)/services/_components/ObjectivesList.tsx
@@ -1,4 +1,4 @@
-import { ChevronRight, Loader2, Plus, SquareDashed } from "lucide-react";
+import { ChevronRight, Plus, SquareDashed } from "lucide-react";
 
 import { Badge } from "@/shared/components/ui/badge";
 import { Button } from "@/shared/components/ui/button";
@@ -6,14 +6,11 @@ import { ScrollArea } from "@/shared/components/ui/scroll-area";
 import { Separator } from "@/shared/components/ui/separator";
 import { cn } from "@/shared/lib/utils";
 import { useDialogStore } from "@/shared/stores/useDialogStore";
-import { useObjectivesByServiceId } from "../_hooks/useObjectives";
 import { useServiceStore } from "../_hooks/useServiceStore";
 import CardItem from "./CardItem";
 
 export default function ObjectivesList() {
   const { selectedService, setSelectedObjective, selectedObjective, clearOnObjectiveDelete } = useServiceStore();
-
-  const { data: objectives, isLoading, error } = useObjectivesByServiceId(selectedService?.id || "");
 
   const { open } = useDialogStore();
 
@@ -29,55 +26,48 @@ export default function ObjectivesList() {
         </Button>
       </div>
       <Separator />
-      {error && <div className="flex items-center justify-center h-full">{error.message}</div>}
-      {isLoading ? (
-        <div className="flex items-center justify-center h-full">
-          <Loader2 className="w-4 h-4 animate-spin" />
-        </div>
-      ) : (
-        <ScrollArea className="flex-1 h-full">
-          <div className="flex flex-col gap-2 p-2">
-            {selectedService ? (
-              objectives?.length && objectives?.length > 0 ? (
-                objectives?.map((objective) => (
-                  <CardItem
-                    key={objective.id}
-                    title={objective.name}
-                    badge={<Badge variant="outline">{objective.activities?.length} Actividades</Badge>}
-                    description={objective.description ?? ""}
-                    onClick={() => setSelectedObjective(objective)}
-                    onEdit={() => open("objectives", "edit", objective)}
-                    onDelete={() => {
-                      clearOnObjectiveDelete(objective.id);
-                      open("objectives", "delete", objective);
-                    }}
-                    className={cn(
-                      selectedObjective?.id === objective.id && "border-sky-400  outline-4 outline-sky-300/10"
-                    )}
-                  />
-                ))
-              ) : (
-                <div className="text-center py-12 px-4">
-                  <div className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-muted mb-4">
-                    <SquareDashed className="h-6 w-6 text-muted-foreground" />
-                  </div>
-                  <h3 className="text-muted-foreground text-xs">No hay objetivos</h3>
-                </div>
-              )
+      <ScrollArea className="flex-1 h-full">
+        <div className="flex flex-col gap-2 p-2">
+          {selectedService ? (
+            selectedService.objectives?.length && selectedService.objectives?.length > 0 ? (
+              selectedService.objectives?.map((objective) => (
+                <CardItem
+                  key={objective.id}
+                  title={objective.name}
+                  badge={<Badge variant="outline">{objective.activities?.length} Actividades</Badge>}
+                  description={objective.description ?? ""}
+                  onClick={() => setSelectedObjective(objective)}
+                  onEdit={() => open("objectives", "edit", objective)}
+                  onDelete={() => {
+                    clearOnObjectiveDelete(objective.id);
+                    open("objectives", "delete", objective);
+                  }}
+                  className={cn(
+                    selectedObjective?.id === objective.id && "border-sky-400  outline-4 outline-sky-300/10"
+                  )}
+                />
+              ))
             ) : (
               <div className="text-center py-12 px-4">
                 <div className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-muted mb-4">
-                  <ChevronRight className="h-6 w-6 text-muted-foreground" />
+                  <SquareDashed className="h-6 w-6 text-muted-foreground" />
                 </div>
-                <h3 className="text-lg font-semibold">Selecciona un servicio</h3>
-                <p className="text-muted-foreground mt-2">
-                  Selecciona un servicio desde el lado izquierdo para ver sus objetivos
-                </p>
+                <h3 className="text-muted-foreground text-xs">No hay objetivos</h3>
               </div>
-            )}
-          </div>
-        </ScrollArea>
-      )}
+            )
+          ) : (
+            <div className="text-center py-12 px-4">
+              <div className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-muted mb-4">
+                <ChevronRight className="h-6 w-6 text-muted-foreground" />
+              </div>
+              <h3 className="text-lg font-semibold">Selecciona un servicio</h3>
+              <p className="text-muted-foreground mt-2">
+                Selecciona un servicio desde el lado izquierdo para ver sus objetivos
+              </p>
+            </div>
+          )}
+        </div>
+      </ScrollArea>
     </div>
   );
 }
