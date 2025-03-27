e6c571627fa07d4db97f760ac6b69f2bb4d55a3e
JMNRA <jmedinan@unsa.edu.pe>
2025-03-27 12:11:29 -0500

    feat: agregar tipos de cotización para la interfaz de administración

---
+++ src/app/(admin)/quotation/_types/quotation.types.ts
@@ -0,0 +1,9 @@
+import { components } from "@/lib/api/types/api";
+
+export type QuotationResponse = components["schemas"]["QuotationResponseDto"];
+
+export type QuotationCreate = components["schemas"]["CreateQuotationDto"];
+
+export type QuotationUpdate = components["schemas"]["UpdateQuotationDto"];
+
+export type QuotationConcrete = components["schemas"]["ConcreteQuotationDto"];
