94beebe6c6e3732a0ffcd9d7d4f42753a17f8121
EleoRomario <eleo.romario@gmail.com>
2025-04-29 17:26:27 -0500

    refactor: integrar ProtectedComponent en QuotationPrimaryButtons y QuotationTableActions
     para gestionar permisos de creación, edición y eliminación de cotizaciones

--- src/app/(admin)/quotation/_components/QuotationTableActions.tsx
+++ src/app/(admin)/quotation/_components/QuotationTableActions.tsx
@@ -2,6 +2,7 @@
 
 import { Edit, MoreHorizontal, Trash } from "lucide-react";
 
+import { ProtectedComponent } from "@/auth/presentation/components/ProtectedComponent";
 import { Button } from "@/shared/components/ui/button";
 import {
   DropdownMenu,
@@ -12,6 +13,7 @@ import {
 } from "@/shared/components/ui/dropdown-menu";
 import { useDialogStore } from "@/shared/stores/useDialogStore";
 import { QuotationResponse } from "../_types/quotation.types";
+import { EnumAction, EnumResource } from "../../roles/_utils/groupedPermission";
 
 interface QuotationTableActionsProps {
   quotation: QuotationResponse;
@@ -32,28 +34,43 @@ export default function QuotationTableActions({ quotation }: QuotationTableActio
   };
 
   return (
-    <div className="flex items-center gap-2">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="bg-background" size="icon">
-            <MoreHorizontal className="size-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem className="cursor-pointer" onClick={handleEdit}>
-            Editar
-            <DropdownMenuShortcut>
-              <Edit className="size-4 mr-2" />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="cursor-pointer" onClick={handleDelete} disabled={quotation.isConcrete}>
-            Eliminar
-            <DropdownMenuShortcut>
-              <Trash className="size-4 mr-2" />
-            </DropdownMenuShortcut>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+    <ProtectedComponent
+      requiredPermissions={[
+        { resource: EnumResource.quotations, action: EnumAction.update },
+        { resource: EnumResource.quotations, action: EnumAction.delete },
+      ]}
+    >
+      <div className="flex items-center gap-2">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="bg-background" size="icon">
+              <MoreHorizontal className="size-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <ProtectedComponent
+              requiredPermissions={[{ resource: EnumResource.quotations, action: EnumAction.update }]}
+            >
+              <DropdownMenuItem className="cursor-pointer" onClick={handleEdit}>
+                Editar
+                <DropdownMenuShortcut>
+                  <Edit className="size-4 mr-2" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </ProtectedComponent>
+            <ProtectedComponent
+              requiredPermissions={[{ resource: EnumResource.quotations, action: EnumAction.delete }]}
+            >
+              <DropdownMenuItem className="cursor-pointer" onClick={handleDelete} disabled={quotation.isConcrete}>
+                Eliminar
+                <DropdownMenuShortcut>
+                  <Trash className="size-4 mr-2" />
+                </DropdownMenuShortcut>
+              </DropdownMenuItem>
+            </ProtectedComponent>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </ProtectedComponent>
   );
 }
